9248dda92711ddbf7ad1b36037d755c0010fdb19
Mernante Tullaga <49087021+mtullaga@users.noreply.github.com>
2021-06-19 15:36:09 +0800

    firebase firestore added

---
+++ www/js/db.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCyI_HHLsKxS-r2Qpet0gMqnlqJhiHXGu8",
+    authDomain: "sari-sari-3a3d2.firebaseapp.com",
+    projectId: "sari-sari-3a3d2",
+    storageBucket: "sari-sari-3a3d2.appspot.com",
+    messagingSenderId: "1054865246427",
+    appId: "1:1054865246427:web:3b3c39e2f770da2acfe4fe",
+    measurementId: "G-THQN1CX7NC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
--- www/js/index.js
+++ www/js/index.js
@@ -1,193 +1,79 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
+
+document.addEventListener("backbutton", onBackKeyDown, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
 
-    // -----------------------------------------------------------------------------
-    // Server IP address for database;
+	let db = firebase.firestore()
 
-    localStorage.setItem('serverAddress', 'https://mdtsecs.000webhostapp.com/');
-    // localStorage.setItem('serverAddress', 'http://localhost/');
-   
 
-	$('#num1').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
+	// Vue Code Starts Here
 
-	$('#num2').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
+	new Vue({
+		el: '#app',
+		data: {
+		  pin: '',
+		  users:[],
+		},
 
-	$('#num3').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
+		methods:{
+			clkNum: function(val){
+				this.pin = this.pin+val
+				ons.notification.prompt('What is your name?')
+			},
+			
+			clkCheck: function(){
 
-	$('#num4').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
+			},
 
-	$('#num5').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
+			clkBackspace: function(){
 
-	$('#num6').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
+			}
+		},
 
-	$('#num7').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
+		created(){
+			db.collection("users")
+				.orderBy('name')
+				.get()
+				.then(querySnapshot => 
+				querySnapshot.docs.map(doc => {
+					let data = doc.data()
+					return {
+					id: doc.id,
+					name: data.name,
+					}
+				})
+				)
+				.then((users) => {
+					console.log(users)
+					
+				})
+		}
+	  })
 
-	$('#num8').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
-
-	$('#num9').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
-
-	$('#num0').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal + $(this).text();
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
-
-	$('#numx').click(function () {
-	    var currentVal = $('#passinput').val();
-	    var newVal = currentVal.slice(0, currentVal.length-1);
-	    $('#passinput').val(newVal);
-	    if(newVal.length == 4){
-	    	verifyPin();
-	    }
-	});
-
-	$('#numcheck').click(function () {
-	    // if($('#passinput').val() == '08'){
-		// 		window.location.href = "components/admin/main.html";
-		// }else{
-		// 	ons.notification.alert('Sorry, invalid pin. Please try again.');
-		// 	$('#passinput').val('');
-		// }
-
-		window.location.href = "components/admin/main.html"
-	});
 
 	
-
-
-	function verifyPin(){
-		var ipaddress = localStorage.getItem('serverAddress');
-		var pass = $('#passinput').val();
-
-		$.post(ipaddress+'/saridb/getcontent.php', {
-            data: 'checkLogin',
-            pin: pass
-          }, function(res){
-          		if(res == 'No records'){
-          			ons.notification.alert('Sorry, invalid pin. Please try again.');
-					$('#passinput').val('');
-          		}else{
-          			var id = JSON.parse(res)[0][0];
-	             	var name = JSON.parse(res)[0][1];
-	             	var pos = JSON.parse(res)[0][2];
-
-	             	if(pos == 'admin'){
-	             		window.location.href = "components/admin/main.html";
-	             	}else{
-	             		window.location.href = "components/staff/main.html";	
-	             	}
-          		}
-
-            })
 		
+}
+
+
+
+function onBackKeyDown() {
+	var res = confirm('Are you sure you want to exit app?');
+	if(res == 1){
+		alert('Application closed.');
+	}else{
+		alert('No actions needed.');
 	}
-
-
-	document.addEventListener("backbutton", onBackKeyDown, false);
-
-	function onBackKeyDown() {
-	    var res = confirm('Are you sure you want to exit app?');
-	    if(res == 1){
-	    	alert('Application closed.');
-	    }else{
-	    	alert('No actions needed.');
-	    }
-	}
+}
 
 
     	
-}
+
    
    
 
